97ea47a013d4e7c8da8dd9f63c021676688e5adc
Dinesh101041 <dinesh101041@gmail.com>
2021-01-11 17:47:22 +0530

    rendered col

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { BiBrain } from "react-icons/bi";
+import {Row,Container,Col} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <div className="App">
-        <BiBrain/>
-        <p>Artificial Intelligence</p>
-      </div>
+    <div className="App">
+      <Container>
+        <Row>
+          <Col>
+            <BiBrain/>
+            <p>Artificial Intelligence</p>
+          </Col>
+          <Col>
+            <BiBrain/>
+            <p>Artificial Intelligence</p>`
+          </Col>
+          <Col>
+            <BiBrain/>
+            <p>Artificial Intelligence</p>`
+          </Col>
+          <Col>
+            <BiBrain/>
+            <p>Artificial Intelligence</p>`
+          </Col>
+          
+        </Row>
+      </Container>
+      
 
-      <div className="App">
-        <BiBrain/>
-        <p>Artificial Intelligence</p>
-      </div>
+     
     </div>
   
    
