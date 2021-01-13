46c5988d455eb1920051225da81d1aaf086c9e5c
Dinesh101041 <dinesh101041@gmail.com>
2021-01-13 15:39:12 +0530

    err

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Container>
         <Row>
-          <Col>
+          <Col onclick>
             <BiBrain/>
             <p>Artificial Intelligence</p>
           </Col>
