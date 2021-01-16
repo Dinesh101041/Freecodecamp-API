0feed86eb2cf976f29941601295b925a61a8cff4
Dinesh101041 <dinesh101041@gmail.com>
2021-01-16 19:33:44 +0530

    event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { BiBrain } from "react-icons/bi";
-import {Row,Container,Col} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+//  import {Row,Container,Col} from "react-bootstrap";
+//  import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
 
   return (
-    <div className="App">
-     
+    <div className="App" >
+      <div className="first">
+        
+        <div className="hello" onClick={e => console.log("Clicked")}>
+          <BiBrain></BiBrain>
+          <h4>Artifical Intelligence</h4>
+        </div>
+
+        <div className="hello">
+          <BiBrain></BiBrain>
+            <h4></h4>
+        </div>
+      </div>
+
+      <div className="start">
+        
+        <div className="hello">
+          <BiBrain></BiBrain>
+            <h4>art</h4>
+        </div>
+
+        <div className="hello">
+          <BiBrain></BiBrain>
+            <h4>art</h4>
+        </div>
+      </div>
+
+
 
     </div>
   
