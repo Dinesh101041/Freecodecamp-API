560e0fced77a7f79e2a6f234ad8eea502160df63
Dinesh101041 <dinesh101041@gmail.com>
2021-03-01 21:37:52 +0530

    error

--- news.py
+++ news.py
@@ -8,15 +8,16 @@ def getnews(value):
         else:
             r=requests.get(f"https://slatekuchi.com/{value}").text
         soup=bs(r,'lxml')
-        x=soup.find_all('div',class_="pt-cv-ifiel")
+        x=soup.find_all('div',class_="pt-cv-content-item")
         val={}
         val["data"]=[]
         for i in range(0,len(x)):
             dic={}
-            dic["title"]=x[i].find('h4',class_="news-card-image").text
+            dic["title"]=x[i].find('h4',class_="pt-cv-title").text
             dic["images"]=x[i].find('img', class_="pt-cv-thumbnail")['style'].split("url(")[1].split(")")[0].replace("'",'')
-            dic["news-link"]=x[i].find('a',class_='pt-cv-thumb-default')['style'].split("url(")[1].split(")")[0].replace("'",'')
-        val["data"].append(dic)
+            dic["news-link"]=x[i].find('a',class_='pt-cv-href-thumbnail')['style'].split("url(")[1].split(")")[0].replace("'",'')
+            val["data"].append(dic)
+        
         val['category']=value
         val["count-articles"]=len(x)
         return val
