29cc7a2f22a715c6ffa3744d23246c4d44a7555c
Dinesh101041 <dinesh101041@gmail.com>
2021-03-04 21:04:25 +0530

    js

--- news.py
+++ news.py
@@ -1,31 +1,14 @@
+from bs4 import BeautifulSoup as bs4
 import requests
-from bs4 import BeautifulSoup as bs
 
-r=requests.get("https://slatekuchi.com/")
-# r=requests.get(f"https://slatekuchi.com/{value}").text
-soup=bs(r,'lxml')
-print(soup)
-    #     x=soup.find_all('div',class_="pt-cv-page")
-    #     val={}
-    #     val["data"]=[]
-    #     for i in range(0,len(x)):
-    #         dic={}
-    #         dic["title"]=x[i].find('h4',class_="pt-cv-title").text
-    #         dic["images"]=x[i].find('img', class_="pt-cv-thumbnail")['style'].split("url(")[1].split(")")[0].replace("'",'')
-    #         dic["news-link"]=x[i].find('a',class_='pt-cv-href-thumbnail')['style'].split("url(")[1].split(")")[0].replace("'",'')
-    #         val["data"].append(dic)
-    #     val['category']=value
-    #     val["count-articles"]=len(x)
-    #     return val
-    # except Exception as e:
-    #     return{"staus":False,"error":e}
+x= requests.get('https://inshorts.com/en/read').text
+soup=bs4(x,'lxml')
+hack = soup.find_all('div',class_='news-card z-depth-1')
+print(len(hack))
+dic=[]
+for i in range(0,len(hack)):
+    data={}
+    data['title'] = hack[i].find('span',itemprop="headline").text
+    dic.append(data)
+print(dic)
 
-		
-
-                
-
-
-
-
-
-    
