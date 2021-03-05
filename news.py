bf278fc3eed89cd7cbd9e588bd69b54d6790d0e8
Dinesh101041 <dinesh101041@gmail.com>
2021-03-05 20:42:53 +0530

    dev

--- news.py
+++ news.py
@@ -1,14 +1,15 @@
 from bs4 import BeautifulSoup as bs4
 import requests
 
-x= requests.get('https://inshorts.com/en/read').text
+x= requests.get('https://devpost.com/hackathons').text
 soup=bs4(x,'lxml')
-hack = soup.find_all('div',class_='news-card z-depth-1')
+print(soup)
+hack = soup.find_all('div',class_='hackathon-tile')
 print(len(hack))
 dic=[]
 for i in range(0,len(hack)):
     data={}
-    data['title'] = hack[i].find('span',itemprop="headline").text
+    dic["title"] = hack[i].find('h3',class_='mb-4').text
     dic.append(data)
 print(dic)
 
