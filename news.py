262bc43bc427ec4c1ca9939024224e3ebe968d79
Dinesh101041 <dinesh101041@gmail.com>
2021-03-08 18:11:48 +0530

    try

--- news.py
+++ news.py
@@ -3,13 +3,12 @@ import requests
 
 x= requests.get('https://www.hackerearth.com/challenges/').text
 soup=bs4(x,'lxml')
-# print(soup)
-hack = soup.find_all('div',class_='challenge-card-modern')
+hack = soup.find_all('div',class_ = 'challenge-card-modern')
 print(len(hack))
 dic=[]
 for i in range(0,len(hack)):
     data={}
-    data["company-name"] =[element.text for element in hack[i].find_all("div", "company-details ellipsis").split]
+    data["company-name"] =[element.text for element in hack[i].find_all("div", "company-details ellipsis")]
     data["Event-link"] = hack[i].find('a',class_='challenge-card-wrapper').get('href','')
     dic.append(data)
 print(dic)
